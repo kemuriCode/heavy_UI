c7d27e35111f0ce151fa10ba0c66191b7525e2e1
t0shi <marcindymek24@gmail.com>
2019-03-13 22:15:20 +0100

    edit viewModel

--- people_table/scripts/PeopleTableViewModel.js
+++ people_table/scripts/PeopleTableViewModel.js
@@ -6,9 +6,24 @@ function PepopleTableViewModel(config) {
   self.contex = config.centext;
 
   self.next = function () {
-    alert('kliknąłeś następny');
-  }
-  self.prev = function () {
-    alert('kliknąłeś następny');
+    self.people.clear();
+    var begin = (sel.currnetPage) + self.pageSize;
+    var end = (self.currnetPage + 1) + self.paeSize;
+    getData(begin, end);
+    self.currnetPage++;
+    self.context.innerHTML = self.people.toTable();
+}
+self.prev = function () {
+    self.people.clear();
+    if (self.currnetPage - 1 >= 0) {
+        self.currnetPage--;
+    }
+    var begin = (selg.currnetPage) + self.paeSize;
+    var end = (self.currnetPage)*self.pageSIze;
+    getData(begin,end);
+    self.context.innerHTML=self.people.toTable();
+}
+  self.pagination = function () {
+      
   }
 }
--- people_table/scripts/scripts.js
+++ people_table/scripts/scripts.js
@@ -16,25 +16,6 @@ var getData = function (begin, end) {
     }
 }
 
-self.next = function () {
-    self.people.clear();
-    var begin = (sel.currnetPage) + self.pageSize;
-    var end = (self.currnetPage + 1) + self.paeSize;
-    getData(begin, end);
-    self.currnetPage++;
-    self.context.innerHTML = self.people.toTable();
-}
-self.prev = function () {
-    self.people.clear();
-    if (self.currnetPage - 1 >= 0) {
-        self.currnetPage--;
-    }
-    var begin = (selg.currnetPage) + self.paeSize;
-    var end = (self.currnetPage)*self.pageSIze;
-    getData(begin,end);
-    self.context.innerHTML=self.people.toTable();
-}
-
 function init() {
     var listOfPeople = new ListOfPeople();
     for (var i = 0; i < data.length; i++) {
