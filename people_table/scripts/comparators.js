5941df77db37c8aa8133e2da65aafc7f413ceed7
t0shi <marcindymek24@gmail.com>
2019-03-14 22:48:34 +0100

    add class comparators

---
+++ people_table/scripts/comparators.js
@@ -0,0 +1,14 @@
+function Comparators() {
+ var self = this;
+ self.byName = function (person1, person2) {
+  return person.firstName.localeCompare(person2.firstName);
+ }
+
+ self.bySurname = function (person1, person2) {
+  return person1.lastName.localeCompare(person2.lastName);
+ }
+
+ self.byAge = function (person1, person2) {
+  return person1.age - person2.age
+ }
+}
